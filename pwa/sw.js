4f430576de6c77e950723080fc84a8675f61f5c2
raisudeep66 <raisudeep66@gmail.com>
2022-03-04 15:55:02 +0530

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -7,7 +7,8 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
+     // return cache.addAll(filesToCache);
+	  return null;
     })
   );
   self.skipWaiting();
